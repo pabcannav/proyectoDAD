aff36bae7e0af70f41e90bba16fd019e33454a84
pabcannav <pabcannav@alum.us.es>
2024-04-23 21:36:15 +0200

    Entregable 1

--- Servlet/src/main/java/es/us/lsi/dad/DHT11.java
+++ Servlet/src/main/java/es/us/lsi/dad/DHT11.java
@@ -1,15 +1,21 @@
 package es.us.lsi.dad;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class DHT11 {
+
+	private int idSensor;
 	private double temperatura;
 	private double humedad;
-	
-	public DHT11(double temperatura, double humedad) {
-		super();
-		this.temperatura = temperatura;
-		this.humedad = humedad;
+
+	public int getIdSensor() {
+		return idSensor;
+	}
+
+	public void setIdSensor(int idSensor) {
+		this.idSensor = idSensor;
 	}
 
 	public double getTemperatura() {
@@ -28,9 +34,23 @@ public class DHT11 {
 		this.humedad = humedad;
 	}
 
+	public List<Double> getValores() {
+		List<Double> res = new ArrayList<>();
+		res.add(temperatura);
+		res.add(humedad);
+		return res;
+	}
+
+	public DHT11(int idSensor, double temperatura, double humedad) {
+		super();
+		this.idSensor = idSensor;
+		this.temperatura = temperatura;
+		this.humedad = humedad;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(humedad, temperatura);
+		return Objects.hash(humedad, idSensor, temperatura);
 	}
 
 	@Override
@@ -42,10 +62,13 @@ public class DHT11 {
 		if (getClass() != obj.getClass())
 			return false;
 		DHT11 other = (DHT11) obj;
-		return Double.doubleToLongBits(humedad) == Double.doubleToLongBits(other.humedad)
+		return Double.doubleToLongBits(humedad) == Double.doubleToLongBits(other.humedad) && idSensor == other.idSensor
 				&& Double.doubleToLongBits(temperatura) == Double.doubleToLongBits(other.temperatura);
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "DHT11 [idSensor=" + idSensor + ", temperatura=" + temperatura + ", humedad=" + humedad + "]";
+	}
+
 }
